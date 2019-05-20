c36799db5d7cc8f9bbf17d08693dc47c6638dd2d
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-05-20 17:27:10 +0900

    add Index.vue

---
+++ static/components/web/Index.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="container">
+        <div class="jumbotron">
+            <h1 class="display-4">C++ Google Cloud Run Example!</h1>
+            <p class="lead">Very Simple Chat App!</p>
+            <hr class="my-4">
+            <p>Developed by C++/Stmilus/Vue.js/Docker/Google Cloud Run</p>
+        </div>
+    </div>    
+</template
