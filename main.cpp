33c4ee73a34d32adfece3fd2aefb900506286260
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-05-20 14:11:35 +0900

    add main.cpp

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include "cpp-httplib/httplib.h"
+
+int main() {
+
+    httplib::Server svr;
+
+    svr.Get("/", [](const httplib::Request& req, httplib::Response& res){
+        res.set_content("Hello C++ on Google Cloud Run!", "text/html");
+    });
+
+    svr.listen("0.0.0.0", 3000);
+}
