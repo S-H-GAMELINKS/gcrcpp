5e9d45a864805c20f5de5e5a12ddc9fbf292827a
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-05-20 15:19:06 +0900

    add vue alias

--- static/webpack.config.js
+++ static/webpack.config.js
@@ -3,5 +3,10 @@ module.exports = {
     output: {
         filename: 'index.js',
         path: `${__dirname}`
+    },
+    resolve: {
+        alias: {
+            'vue$': 'vuew/dist/vue.esm.js'
+        }
     }
 }
