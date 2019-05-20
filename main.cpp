19f69df1f84b6d2bf59c4f69ff16f03f07350ce8
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-05-20 15:50:24 +0900

    using /index.js route

--- main.cpp
+++ main.cpp
@@ -20,8 +20,14 @@ int main() {
 
     const std::string html = load_assets("./static/index.html");
 
+    const std::string js = load_assets("./static/index.js");
+
     svr.Get("/", [&](const httplib::Request& req, httplib::Response& res){
         res.set_content(html, "text/html");
+    });
+
+    svr.Get("/index.js", [&](const httplib::Request& req, httplib::Response& res){
+        res.set_content(js, "text/javascript");
     });
 
     svr.listen("0.0.0.0", 8080);
