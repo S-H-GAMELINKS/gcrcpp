c3d93c225848de4d5dac361c5e031a911dad601f
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-05-20 14:58:01 +0900

    using static/index.html

--- main.cpp
+++ main.cpp
@@ -1,11 +1,27 @@
 #include "cpp-httplib/httplib.h"
+#include <fstream>
+#include <sstream>
+#include <string>
+
+const std::string load_assets(const std::string& path) {
+    std::ifstream static_file(path.c_str(), std::ios::in);
+
+    std::stringstream stream;
+
+    stream << static_file.rdbuf();
+    static_file.close();
+
+    return stream.str();
+}
 
 int main() {
 
     httplib::Server svr;
 
-    svr.Get("/", [](const httplib::Request& req, httplib::Response& res){
-        res.set_content("Hello C++ on Google Cloud Run!", "text/html");
+    const std::string html = load_assets("./static/index.html");
+
+    svr.Get("/", [&](const httplib::Request& req, httplib::Response& res){
+        res.set_content(html, "text/html");
     });
 
     svr.listen("0.0.0.0", 8080);
