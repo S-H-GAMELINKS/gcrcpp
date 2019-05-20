5995c16ddfc46f9c85e5b14ba8c847666bedff76
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-05-20 17:28:39 +0900

    add Contact.vue

---
+++ static/components/web/Contact.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="container">
+        <div class="jumbotron">
+            <h1 class="display-4">Contact</h1>
+            <p class="lead">S.H.</p>
+            <hr class="my-4">
+            <p class="lead">mail: gamelinks007@gmail.com</p>
+            <p class="lead">github: <a href="https://github.com/S-H-GAMELINKS">S-H-GAMELINKS</a></p>
+            <p class="lead">
+                <a class="btn btn-primary btn-lg" href="https://github.com/S-H-GAMELINKS/gcrcpp" role="button">Source</a>
+            </p>
+        </div>
+    </div>    
+</template>
